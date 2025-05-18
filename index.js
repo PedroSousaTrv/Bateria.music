27beb99e0df7d78df5d93fe33c32f66bddc4fe2b
Pedro  de Sousa <pedrosousa2003@gmail.com>
2025-05-17 21:00:24 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,54 @@
+document.body.addEventListener("keyup", (event)=>{
+    playsong(event.code.toLowerCase());
+});
+
+document.querySelector(".inputs button").addEventListener("click", ()=>{
+    let song = document.querySelector("#input").value;
+
+    if (song !== "" ){
+        let songArray = song.split("");
+        playcomposition(songArray);
+    }
+});
+
+document.querySelectorAll(`div[data-key]`).forEach((keyElement)  => {
+    keyElement.addEventListener("click", ()=>{
+    let key = keyElement.getAttribute("data-key");
+    playsong(key);
+    })
+})
+
+
+function playcomposition(songArray){
+    let wait = 0;
+
+    for( let songItem of songArray){
+        setTimeout(() => {
+            playsong(`key${songItem}`)
+        }, wait);
+
+        wait += 250;    
+    }
+
+}
+
+function playsong(sound){
+    let audioElement = document.querySelector(`#s_${sound}`)
+    let keyElement = document.querySelector(`div[data-key="${sound}"]`)
+
+
+    if (audioElement){
+        audioElement.currentTime = 0;
+        audioElement.play();
+    }
+
+    if (keyElement){
+        keyElement.classList.add("active")
+    }
+
+    setTimeout(() => {
+       keyElement.classList.remove("active") 
+    }, 300);
+
+
+}
